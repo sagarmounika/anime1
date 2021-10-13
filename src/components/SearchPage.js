a71a7e23df22e46ea308c4eff84816f57cb466f5
sagarmounika <sagarmouni5@gmail.com>
2021-10-13 13:18:16 +0530

    Add files via upload

---
+++ src/components/SearchPage.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react"
+
+import React from "react"
+
+function Search(props) {
+  const { data } = props
+  const [input, setInput] = useState("")
+
+  const searchHandler = e => {
+    const val = e.target.value
+    setInput(val)
+  }
+ 
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search Anime here"
+        onChange={searchHandler}
+      />
+
+      <button >Search</button>
+
+    </div>
+  )
+}
+
+export default Search
